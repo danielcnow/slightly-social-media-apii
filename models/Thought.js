80b486795acfa6c70cb7a1050d9d5dad93c08f2f
danielcnow <105885467+danielcnow@users.noreply.github.com>
2022-10-19 00:27:41 -0400

    models

---
+++ models/Thought.js
@@ -0,0 +1,36 @@
+const thoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
+      username: [
+        {
+          type: string,
+          required: true,
+        },
+      ],
+      reactions: [reactionSchema],
+    },
+    {
+      toJSON: {
+        getters: true,
+      },
+      id: false,
+    }
+  );
+
+  thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+  });
+  
+  const Thought = model("Thought, thoughtSchema");
+
+  module.exports = Thought;
